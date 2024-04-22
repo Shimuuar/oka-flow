d96955c3639fcab0a18b8295ede73291512d7b71
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2024-04-22 17:29:15 +0300

    Add cabal.project & update shell.nix

--- shell.nix
+++ shell.nix
@@ -8,9 +8,42 @@ let
     oka-nix.hackage
     oka-nix.overlay
   ];
+  #--
+  pkgs_hask = pkgs.oka.haskell94.ghcWithPackages (p: with p; [
+     aeson
+     async
+     base16-bytestring
+     bytestring
+     containers
+     cryptohash-sha1
+     directory
+     filepath
+     fixed-vector
+     generic-arbitrary
+     lens
+     mtl
+     operational
+     process
+     quickcheck-instances
+     scientific
+     stm
+     tasty
+     tasty-hunit
+     tasty-quickcheck
+     temporary
+     text
+     these
+     time
+     transformers
+     typed-process
+     unix
+     unordered-containers
+     vector
+     yaml
+  ]);
 in
-pkgs.oka.haskell94.shellFor {
-  packages = hs: [
-    (hs.callCabal2nix "oka-metadata" ./. {})
+pkgs.mkShell {
+  buildInputs = [
+    pkgs_hask
   ];
 }
