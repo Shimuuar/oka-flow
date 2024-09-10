d56920cca8dc753da6045c0f857979673dd5e9a0
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2024-09-10 11:40:58 +0300

    Add monadic variant of a parser

--- oka-flow/OKA/Flow/Parser.hs
+++ oka-flow/OKA/Flow/Parser.hs
@@ -3,7 +3,9 @@
 -- |
 -- Extremely simple parser for parsing lists
 module OKA.Flow.Parser
-  ( ListParser(..)
+  ( ListParserT(..)
+  , ListParser
+  , runListParserT
   , runListParser
   , consume
   ) where
@@ -12,26 +14,32 @@ import Control.Applicative
 -- import Control.Monad
 import Control.Monad.Except
 import Control.Monad.State.Strict
+import Data.Functor.Identity
 
-
-type role ListParser representational representational
 
 -- | Very simple pure parser which consumes list.
-newtype ListParser s a = ListParser
-  { get :: [s] -> Either String (a, [s])
+newtype ListParserT s m a = ListParserT
+  { run :: [s] -> m (Either String (a, [s]))
   }
-  deriving (Functor, Applicative, Alternative, Monad, MonadError String, MonadState [s])
-       via StateT [s] (Either String)
+  deriving (Functor, Applicative, Alternative, Monad, MonadIO, MonadError String, MonadState [s])
+       via StateT [s] (ExceptT String m)
+
+type ListParser s = ListParserT s Identity
 
 -- | Execute parser. It will fail if arguments are not consumed fully
+runListParserT :: Monad m => ListParserT s m a -> [s] -> m (Either String a)
+runListParserT parser xs =
+  parser.run xs >>= \case
+    Left  e       -> pure $ Left e
+    Right (a, []) -> pure $ Right a
+    Right (_, _ ) -> pure $ Left "runListParser: unconsumed argument"
+
+-- | Execute pure parser.
 runListParser :: ListParser s a -> [s] -> Either String a
-runListParser parser xs =
-  parser.get xs >>= \case
-    (a, []) -> Right a
-    (_, _ ) -> Left "runListParser: unconsumed argument"
+runListParser p = runIdentity . runListParserT p
 
 -- | Consume single element from the input stream
-consume :: ListParser s s
+consume :: Monad m => ListParserT s m s
 consume = get >>= \case
   []   -> throwError "Not enough inputs"
   s:ss -> s <$ put ss
