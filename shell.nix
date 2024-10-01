b341e5f714a48c39cb664f5025cd75d22250f8d2
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2024-10-01 18:38:38 +0300

    shell.nix stopped working with difficult to understand error
    Let do this hard way

--- shell.nix
+++ shell.nix
@@ -8,10 +8,44 @@ let
     oka-nix.hackage
     oka-nix.overlay
   ];
+  #---
+  hask = pkgs.oka.haskell94.ghcWithPackages (p: with p; [
+    aeson
+    async
+    base16-bytestring
+    bytestring
+    containers
+    cryptohash-sha1
+    directory
+    effectful-core
+    filepath
+    fixed-vector
+    generic-arbitrary
+    lens
+    mtl
+    optparse-applicative
+    process
+    quickcheck-instances
+    random
+    scientific
+    stm
+    tasty
+    tasty-hunit
+    tasty-quickcheck
+    temporary
+    text
+    these
+    time
+    transformers
+    typed-process
+    unix
+    unordered-containers
+    vector
+    yaml
+  ]);
 in
-pkgs.oka.haskell94.shellFor {
-  packages = hs: [
-    (hs.callCabal2nix "oka-metadata" ./oka-metadata {})
-    (hs.callCabal2nix "oka-flow"     ./oka-flow     {})
+pkgs.mkShell {
+  buildInputs = [
+    hask
   ];
 }
