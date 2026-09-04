56280ae58b230032a968279e11d1286067894a18
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2026-09-04 20:50:59 +0300

    Add JSON instances for StorePath & Hash

--- oka-flow/tests/TM/Serialization.hs
+++ oka-flow/tests/TM/Serialization.hs
@@ -1,19 +1,27 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 -- |
 {-# OPTIONS_GHC -Wno-orphans #-}
 module TM.Serialization
   ( tests
   ) where
 
+import Crypto.Hash.SHA1             qualified as SHA1
+import Data.Aeson                   qualified as JSON
+import Data.ByteString.Lazy         qualified as BL
+import Data.Typeable
 import Test.Tasty
 import Test.Tasty.QuickCheck
 
 import OKA.Flow.Core.S
+import OKA.Flow.Core.Types
 import OKA.Flow.Tools
 
 tests :: TestTree
 tests = testGroup "Serialization"
   [ testProperty "roundtrip S args" args_roundtrip
   , testProperty "roundtrip S JSON" json_roundtrip
+  , testJsonRoundtrip @StorePath
+  , testJsonRoundtrip @Hash
   ]
 
 
@@ -24,6 +32,11 @@ args_roundtrip s
 json_roundtrip :: S Int -> Bool
 json_roundtrip s
   = Right s == (sFromJSON . sToJSON) s
+
+testJsonRoundtrip :: forall a. (JSON.ToJSON a, JSON.FromJSON a, Arbitrary a, Eq a, Show a, Typeable a) => TestTree
+testJsonRoundtrip
+  = testProperty ("JSON: " ++ show (typeOf (undefined :: a)))
+  $ \(s::a) -> Right s == (JSON.eitherDecode . JSON.encode) s
 
 ----------------------------------------------------------------
 -- Orphans
@@ -46,6 +59,11 @@ instance Arbitrary (S FilePath) where
   arbitrary = liftArbitrary $ vectorOf 10 (choose ('a', 'z'))
 instance Arbitrary (S Int) where
   arbitrary = liftArbitrary arbitrary
+
+instance Arbitrary StorePath where
+  arbitrary = StorePath <$> atom <*> arbitrary
+instance Arbitrary Hash where
+  arbitrary = Hash . SHA1.hashlazy . BL.pack <$> arbitrary
 
 atom :: Gen FilePath
 atom = vectorOf 8 (choose ('a', 'z'))
