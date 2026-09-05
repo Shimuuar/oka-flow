5bc765c5a6e6ff5e875aca305ecfd45222c9cef6
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2026-09-05 19:44:47 +0300

    Make mypy happy

--- python/okaflow/okaflow/encoding.py
+++ python/okaflow/okaflow/encoding.py
@@ -11,17 +11,17 @@ class SexpSerializer:
     @pydantic.model_serializer
     def _serialize_sexp(self) -> list:
         cls = self.__class__
-        return [cls.__name__]+[getattr(self, f) for f in cls.__pydantic_fields__]
+        return [cls.__name__]+[getattr(self, f) for f in cls.__pydantic_fields__] # type: ignore
 
     @pydantic.model_validator(mode='before')
     @classmethod
     def _parse_sexp(cls, data: Any) -> Any:
-        fields = cls.__pydantic_fields__
+        fields = cls.__pydantic_fields__ # type: ignore
         match data:
             case list():
                 if len(data) != len(fields)+1:
                     raise ValueError("Invalid S-expression length")
                 if data[0] != cls.__name__:
                     raise ValueError(f"Invalid class name {data[0]} expected {cls.__name__}")
-                return {f:x for f,x in zip(cls.__pydantic_fields__, data[1:])}
+                return {f:x for f,x in zip(cls.__pydantic_fields__, data[1:])} # type: ignore
             case _: return data
